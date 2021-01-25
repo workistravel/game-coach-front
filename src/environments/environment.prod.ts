3bf7619773208a6e281c6dccce3a2a5deff2ada1
serhiidernovyi <gifulmen@gmail.com>
2021-01-25 11:31:59 +0100

    commit 26.01.21-2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://coach-back.herokuapp.com',
   defaultPhoto: 'https://gifulmen.blob.core.windows.net/card/19a57100-5010-4af6-8a23-d920895a0100.jpeg',
-  defaultPhotoFront: 'https://gifulmen.blob.core.windows.net/card/3c07c5ec-e962-4048-a59b-69c8e9f7761b.jpeg'
+  defaultPhotoFront: 'https://gifulmen.blob.core.windows.net/card/3c07c5ec-e962-4048-a59b-69c8e9f7761b.jpeg',
+
+
 };
