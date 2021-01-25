88b4b98be17631fef302dd00e1e96e508e8794ba
serhiidernovyi <gifulmen@gmail.com>
2021-01-25 11:13:21 +0100

    commit 26.01.21

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  // apiUrl: 'https://coach-back.herokuapp.com',
+  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://coach-back.herokuapp.com',
   defaultPhoto: 'https://gifulmen.blob.core.windows.net/card/19a57100-5010-4af6-8a23-d920895a0100.jpeg',
   defaultPhotoFront: 'https://gifulmen.blob.core.windows.net/card/3c07c5ec-e962-4048-a59b-69c8e9f7761b.jpeg'
 };
 
-/*
+/*npm install compression —save
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
